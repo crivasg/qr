190866ba403db1cc26bc11620fa73b1128a6dbe6
Cesar A. Rivas <crivasg@gmail.com>
2022-09-15 12:55:52 -0500

    Update cli.py

--- qr/cli.py
+++ qr/cli.py
@@ -1,2 +1,20 @@
 '''cli.py'''
 
+import argparse
+import pathlib
+
+import qr
+
+class CustomFormatter(argparse.RawDescriptionHelpFormatter,
+                      argparse.ArgumentDefaultsHelpFormatter):
+    pass
+
+def read_user_cli_args():
+    '''Handle the CLI arguments and options'''
+    
+    parser = argparse.ArgumentParser(
+        prog='qr',
+        description = qr.__title__,
+        formatter_class=CustomFormatter)
+    
+    return parser.parse_args()
