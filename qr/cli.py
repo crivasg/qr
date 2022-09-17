2b0c9c2e9234e88339c5cb2b37b5a538acb790f0
Cesar A. Rivas <crivasg@gmail.com>
2022-09-16 20:26:42 -0500

    Update cli.py
    added the subparsers decode and encode

--- qr/cli.py
+++ qr/cli.py
@@ -3,11 +3,20 @@
 two function: decode/encode
 
 '''
-
 import argparse
 import pathlib
+from urllib.parse import urlparse
 
 import qr
+
+def url_type(arg):
+
+    url = urlparse(arg)
+    if all((url.scheme, url.netloc, url.path)):  # possibly other sections?
+        return arg  # return url in case you need the parsed object
+    raise ArgumentTypeError('Invalid URL')
+    
+    
 
 class CustomFormatter(argparse.RawDescriptionHelpFormatter,
                       argparse.ArgumentDefaultsHelpFormatter):
@@ -16,9 +25,30 @@ class CustomFormatter(argparse.RawDescriptionHelpFormatter,
 def read_user_cli_args():
     '''Handle the CLI arguments and options'''
     
-    parser = argparse.ArgumentParser(
-        prog='qr',
-        description = qr.__title__,
-        formatter_class=CustomFormatter)
+    parser = argparse.ArgumentParser(prog='qr',
+                                     description = qr.__title__,
+                                     epilog='please send complaints to /dev/null',
+                                     formatter_class=CustomFormatter)
+
+    subparsers = parser.add_subparsers(dest='subparser_name')
+    ## create the parser for the 'decode' subcommand
+    parser_decode = subparsers.add_parser('decode',
+                                          help='Decodes the QR code')
+
+    parser_decode.add_argument('--input',
+                               type=pathlib.Path,
+                               required = False,
+                               help='path to the image containing the QR code')
+
+    ## https://stackoverflow.com/q/73718877
+    parser_decode.add_argument('--url',
+                               type=url_type,
+                               required = False,
+                               help='URL to the image containing the QR code')
+
+    parser_encode = subparsers.add_parser('encode',
+                                          help='Encodes data into a QR code')
+    
+    
     
     return parser.parse_args()
