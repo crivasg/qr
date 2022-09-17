0ecf86ddf059afb7e27d7903164b37629301dead
Cesar A. Rivas <crivasg@gmail.com>
2022-09-16 20:27:27 -0500

    cli arguments
    call the cli arguments

--- qr/__main__.py
+++ qr/__main__.py
@@ -1,7 +1,14 @@
+
 '''__main__.py '''
 
-def main():
-  pass
+from qr.cli import read_user_cli_args
+
+import pathlib
+import sys
 
 if __name__ == '__main__':
-  main()
+    if len(sys.argv)==1:
+        sys.exit(-1)
+
+    args = read_user_cli_args()
+    args.func(args)
