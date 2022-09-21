a92e4425c9b9cc1ee424222b031132ed143a75dd
Cesar A. Rivas <crivasg@gmail.com>
2022-09-21 11:54:01 -0500

    Update __main__.py
    created App class  and imported tkinter

--- qr.gui/__main__.py
+++ qr.gui/__main__.py
@@ -1,7 +1,28 @@
 '''__main__.py'''
 
-def main():
-  pass
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+class App(tk.Tk):
+    
+    def __init__(self):
+        self.resizable(0, 0)
+        self.title('QR Encode/Decode')
+        
+    def create_header_frame(self):
+        pass
+    
+    def create_body_frame(self):
+        '''will contain a notebook with two tabs, encode and decode'''
+        
+        self.notebook = ttk.Notebook(self)
+        self.notebook.pack(padx=5, pady=5, expand=True)
+    
+    def create_footer_frame(self):
+        pass
+        
 
 if __name__ == '__main__':
-  main()
+    app=App()
+    app.mainloop()
